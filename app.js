49d32670b1807dc79b0224585f8e8bdf9364acca
cintyy <cintiapaezdm@gmail.com>
2022-06-22 11:42:36 -0300

    cambio nombre x apellido

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Bienvenido!')
 
 const nombre = prompt('Ingrese su nombre');
-const apellido = prompt('Ingrese su nombre');
+const apellido = prompt('Ingrese su apellido');
 
 alert('Hola' + nombre + apellido + 'Bienvenida a Ada');
